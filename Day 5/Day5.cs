97432c90f71c282010c6395f76af42a948523b97
GAndreou97 <119510932+GAndreou97@users.noreply.github.com>
2023-01-03 11:20:45 +0000

    Progress

--- Day 5/Day5.cs
+++ Day 5/Day5.cs
@@ -16,45 +16,58 @@ namespace AdventOfCode2022{
             char tempCrate = ' ';
             string[] input = File.ReadAllLines(@"/Users/georgeandreou/Documents/GitHub/Advent-Of-Code-2022/Day 5/Input.txt");
             
-            //return input[1][1];
+            //return input[2][1];
 
             foreach (string line in input){ //finds the index of the white space between the two inputs
-                counter++;
                 if (line == ""){
                     indexofLinebreak = counter;
                     break;
                 }
+                counter++;
             }
 
             indexOfColumnNums = indexofLinebreak -1;
             indexOfStartofInstructions = indexofLinebreak +1;
-
             //column[2].Add("what"); Example of how to add to an individual internal list
 
             //goes through the index line and then moves up building up a temp list which is then applied to the list of lists
+             //Console.WriteLine(input[indexOfColumnNums][1]);
+            // Console.WriteLine(input[indexOfStartofInstructions]); 
+            // Console.WriteLine(input[indexofLinebreak]); 
+
             for (int x = 0; x < input[indexOfColumnNums].Length -1; x++){
                 tempstring = input[indexOfColumnNums][x]; 
                 if (tempstring == ' '){ //do nothing
+                //Console.WriteLine("Empty");
+                //Console.WriteLine(input[indexOfColumnNums][x]);
                 }
                 else {
                     indexOfColumnTemp = x;
-                    for(int y = indexOfColumnNums-1; y > 0; y-- ){
+                    //Console.WriteLine(tempstring);
+                    //Console.WriteLine(input[indexOfColumnNums][x]);
+                    for(int y = indexOfColumnNums-1; y >= 0; y-- ){
                         tempCrate = input[y][indexOfColumnTemp];
+                        //Console.WriteLine(input[y][indexOfColumnTemp]);
+                        //Console.WriteLine(y);
                         if (tempCrate != ' ' && tempCrate != '[' && tempCrate != ']'){
                             tempList.Add(Char.ToString(tempCrate));
+                            //Console.WriteLine(tempList[0]);
                         }
                     }
+                    //List<string> tempList2 = new List<string>(tempList);
+                    //so its not WORKING BECAUSE OF LIST REFERENCING
                     column.Add(tempList); //built up one column at a time and adding it to main list of lists
                     tempList.Clear(); //then clears the temp list to go around again
-
+                    Console.WriteLine(column[0][0]);
                 }
-                x++;
+                //x++;
+                //Console.WriteLine(x);
             }     
 
 
 
-            //return input[0][0];
-
+            //return column[0][0];
+            return input[1][1];
            // foreach (string line in System.IO.File.ReadLines(@"/Users/georgeandreou/Documents/GitHub/Advent-Of-Code-2022/Day 4/Input.txt")){}
 
 
